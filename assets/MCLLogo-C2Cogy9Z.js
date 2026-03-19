3d7a4837b52d0b6f02861d4ff5186c0dd046df43
Subrat6869 <Subrat6869@users.noreply.github.com>
2026-03-19 20:42:52 +0530

    Updates

--- assets/MCLLogo-C2Cogy9Z.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-DCcYWASw.js";const t=({className:o=""})=>a.jsx("img",{src:"/mcl-logo.png",alt:"MCL - Mahanadi Coalfields Limited",className:`object-contain ${o}`});export{t as M};
